ad4af2cb6be572cbbbe1f2ef4550fe8d5892a065
Cameron L. <imcelarson@gmail.com>
2026-03-15 23:06:18 -0600

    Passed initial test for 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let charArr = string.split("");
+    let reverseArr = [];
 
+    for (let i = 0; i < string.length; i++) {
+        reverseArr.unshift(charArr[i]);
+    }
+
+    return reverseArr.join("");
 };
 
 // Do not edit below this line
