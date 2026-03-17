65e41ad13a3576fd0302674254c19a03b6cdad9f
Cameron L. <imcelarson@gmail.com>
2026-03-16 22:12:28 -0600

    Passed initial test

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+// F -> C
+const convertToCelsius = function(num) {
+  return Math.round(((num - 32) * (5/9)) * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+// C -> F
+const convertToFahrenheit = function(num) {
+  return Math.round(((num * (9/5)) + 32) * 10) / 10
 };
 
 // Do not edit below this line
