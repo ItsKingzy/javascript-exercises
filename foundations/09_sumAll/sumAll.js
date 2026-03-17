b0acac5f15c4701fce065134de16d7e07ec326b1
Cameron L. <imcelarson@gmail.com>
2026-03-16 21:50:29 -0600

    Testing implemented, tests pass

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,10 @@
 const sumAll = function(int1, int2) {
+    // Check if the num is an integer
+    if (!Number.isInteger(int1) || !Number.isInteger(int2)) return "ERROR";
+    // Check if nums are positive
+    if (int1 < 0 || int2 < 0) return "ERROR";
+    
+    
     let small = int1 < int2 ? int1 : int2;
     let large = small === int1 ? int2 : int1;
     let sum = 0;
