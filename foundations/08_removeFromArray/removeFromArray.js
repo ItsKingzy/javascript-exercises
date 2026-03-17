83eb3f791069d56c2ff11189632c71eefc2fdc03
Cameron L. <imcelarson@gmail.com>
2026-03-16 21:35:25 -0600

    SOlving for a more optimal solution

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function(arr, item) {
-    return arr.filter((element) => element !== item)
+const removeFromArray = function(arr, ...extras) {
+    /*
+    This solution took me a lot of thinking, but 
+    we can filter the list, and if the list has 
+    some element inside the "extras" array (which 
+    represents and array of all optional 
+    arguments), then filter it out.
+    */
+    return arr.filter((item) => !extras.includes(item))
 };
 
 // Do not edit below this line
