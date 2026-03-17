cfd93fda8bfb4a377c4024a498a22b45a7ba3c62
Cameron L. <imcelarson@gmail.com>
2026-03-16 21:45:12 -0600

    Initial solution

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let small = int1 < int2 ? int1 : int2;
+    let large = small === int1 ? int2 : int1;
+    let sum = 0;
 
+    for (let i = small; i <= large; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
