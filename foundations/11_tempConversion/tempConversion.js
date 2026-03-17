3b40808e4df265c64f7e5aa96ba5f72629975eb0
Cameron L. <imcelarson@gmail.com>
2026-03-16 22:14:34 -0600

    Added comment on Math.round() function for floats to 1 decimal place

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,10 +1,12 @@
 // F -> C
 const convertToCelsius = function(num) {
+  // N.B., Math.round(# * 10)/10, returns a float to 1 decimal place
   return Math.round(((num - 32) * (5/9)) * 10) / 10
 };
 
 // C -> F
 const convertToFahrenheit = function(num) {
+  // N.B., Math.round(# * 10)/10, returns a float to 1 decimal place
   return Math.round(((num * (9/5)) + 32) * 10) / 10
 };
 
